e0b2a270f4fed065929db5666ca7695941979c9d
Amenawon Esezobor <menaesezobor@gmail.com>
2021-08-15 23:37:09 +0100

    add the file upload api request and search data request

---
+++ SearchApartments.Core/Models/APIRequests/FileUploadRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace SearchApartments.Core.Models.APIRequests
+{
+    public class FileUploadRequest
+    {
+        [Required(ErrorMessage ="Please upload a file")]
+        public IFormFile PropertyFile { get; set; }
+        [Required(ErrorMessage = "Please upload a file")]
+        public IFormFile ManagementFile { get; set; }
+    }
+}
---
+++ SearchApartments.Core/Models/APIRequests/SearchDataRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchApartments.Core.Models.APIRequests
+{
+    public class SearchDataRequest
+    {   [Required(ErrorMessage ="Search phrase is required")]
+        public string SearchPhrase { get; set; }
+        public string Market { get; set; }
+        public int Limit { get; set; } = 25;
+    }
+}
