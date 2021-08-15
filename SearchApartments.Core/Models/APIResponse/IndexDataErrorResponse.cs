61f2971b6426c6811a69610376d81f2e5546201e
Amenawon Esezobor <menaesezobor@gmail.com>
2021-08-15 23:38:02 +0100

    add the search data response and data response from file upload

---
+++ SearchApartments.Core/Models/APIResponse/IndexDataErrorResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchApartments.Core.Models.APIResponse
+{
+    public class IndexDataErrorResponse
+    {
+        public bool ManagementError { get; set; }
+        public bool PropertyError { get; set; }
+    }
+}
---
+++ SearchApartments.Core/Models/APIResponse/SearchDataResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchApartments.Core.Models.APIResponse
+{
+    public class SearchDataResponse {
+        public Management Mgmt { get; set; }
+        public Property Property { get; set; }
+    }
+}
