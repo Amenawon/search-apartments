ce21a9a392f1ade3c8619e11ea4b1558fbef6f7b
Amenawon Esezobor <menaesezobor@gmail.com>
2021-08-15 23:35:33 +0100

    add a controller to upload files

---
+++ SearchApartments.API/Controllers/FileUploadController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchApartments.Core.Interfaces;
+using SearchApartments.Core.Models.APIRequests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchApartments.API.Controllers
+{
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        readonly IFileUploadService _fileUploadService;
+        public FilesController(IFileUploadService fileUploadService)
+        {
+            _fileUploadService = fileUploadService;
+        }
+
+        [HttpPost("files")]
+        public async Task<IActionResult> Files([FromForm] FileUploadRequest fileUploadRequest)
+        {
+            var response = await _fileUploadService.Uploadfiles(fileUploadRequest);
+            
+            return Ok(response);
+        }
+    }
+}
