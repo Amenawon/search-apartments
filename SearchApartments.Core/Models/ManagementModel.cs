1f4c720ab911987a46b4cf25a2982e3da61da49c
Amenawon Esezobor <menaesezobor@gmail.com>
2021-08-15 23:36:27 +0100

    add a Property model and management model

---
+++ SearchApartments.Core/Models/ManagementModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace SearchApartments.Core.Models
+{
+    public class ManagementModel
+    {
+
+        public Management Mgmt { get; set; }
+    }
+    public class Management
+    {
+        public int MgmtID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Market { get; set; }
+
+        public string State { get; set; }
+    }
+}
---
+++ SearchApartments.Core/Models/PropertyModel.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchApartments.Core.Models
+{
+   public class PropertyModel
+    {
+        public Property Property { get; set; }
+    }
+
+    public class Property
+    {
+        public int PropertyID { get; set; }
+
+        public string Name { get; set; }
+
+        public string FormerName { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string Market { get; set; }
+
+        public string State { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Lng { get; set; }
+    }
+}
