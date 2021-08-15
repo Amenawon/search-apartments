488d64035f2776d8072935e820edc8cb42edbe97
Amenawon Esezobor <menaesezobor@gmail.com>
2021-08-15 23:35:46 +0100

    add a controller to search

---
+++ SearchApartments.API/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchApartments.Core.Interfaces;
+using SearchApartments.Core.Models.APIRequests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchApartments.API.Controllers
+{
+    [ApiController]
+    public class SearchController : Controller
+    {
+        readonly ISearchDataService _searchDataService;
+        public SearchController(ISearchDataService searchDataService)
+        {
+            _searchDataService = searchDataService; 
+        }
+
+        [HttpGet("data")]
+        public async Task<IActionResult> Data([FromQuery] string searchPhrase, string market, int limit=25)
+        {
+             var response = await _searchDataService.SearchData(searchPhrase, market, limit);
+
+            return Ok(response);
+        }
+    }
+}
