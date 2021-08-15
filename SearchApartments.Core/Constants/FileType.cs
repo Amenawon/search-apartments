d24041bc696fffbb80774e3ce3537ebe22020c0e
Amenawon Esezobor <menaesezobor@gmail.com>
2021-08-15 23:36:04 +0100

    add a file type constant

---
+++ SearchApartments.Core/Constants/FileType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchApartments.Core.Constants
+{
+    public sealed class FileType
+    {
+        FileType()
+        {
+
+        }
+        public const string Properties = "properties";
+        public const string Managements = "managements";
+    }
+}
